f5d858a8566904d1cb42132992686a34cf642cfa
Sonja Markovic <sonjamarkovicccc@gmail.com>
2025-01-03 20:24:06 +0100

    dodata funkcija za validnost mjesecne karte

--- src/MjesecnaKartica.h
+++ src/MjesecnaKartica.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctime>
 
 class MjesecnaKartica
 {
@@ -63,9 +64,72 @@ class MjesecnaKartica
             zona[strcspn(zona, "\n")] = '\0';
         }
 
+        bool jeValidna()
+        {
+            struct tm datum;
+            memset(&datum, 0, sizeof(datum));
+
+            char* dan = substr(datumIsteka, 0, 2);
+            char* mjesec = substr(datumIsteka, 2, 2);
+            char* godina = substr(datumIsteka, 2, 2);
+
+            int day = parseInt(dan);
+            int month = parseInt(mjesec);
+            int year = parseInt(godina);
+
+            datum.tm_mday = day;
+            datum.tm_mon = month;
+            datum.tm_year = year;
+
+            time_t istDate = mktime(&datum);
+            time_t now = time(NULL);
+
+            return now > istDate;
+        }
+
         char* getIme () const { return ime;}
         char* getPrezime () const { return prezime;}
         char* getZona () const { return zona;}
         char* getDatumIzdaje () const { return datumIzdaje;}
         char* getDatumIsteka () const { return datumIsteka;}
+
+    private:
+        char* substr(char* str, int start, int length)
+        {
+            if (str == nullptr || start < 0 || length < 0) {
+                return nullptr;
+            }
+
+            int strLength = strlen(str);
+            if (start + length > strLength) {
+                length = strLength - start;
+            }
+
+            char* result = new char[length + 1];
+            strncpy(result, str + start, length);
+            result[length] = '\0';
+
+            return result;
+        }
+
+        int parseInt(char* chars)
+        {
+            int sum = 0;
+            int len = strlen(chars);
+            for (int x = 0; x < len; x++)
+            {
+                int n = chars[len - (x + 1)] - '0';
+                sum = sum + powInt(n, x);
+            }
+            return sum;
+        }
+
+        int powInt(int x, int y)
+        {
+            for (int i = 0; i < y; i++)
+            {
+                x *= 10;
+            }
+            return x;
+        }
 };
