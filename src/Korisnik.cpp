dacde323c5cdc15fe3dbb895e86936da0b522747
Sonja Markovic <sonjamarkovicccc@gmail.com>
2025-01-06 21:46:55 +0100

    dodani fajlovi

---
+++ src/Korisnik.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "UlaznaKartica.h"
+
+int main()
+{
+    // Dobar dan, na parkingu ima X praznih mjesta - Parking.h
+    // Unesite broj tablice
+    UlaznaKartica kartica;
+    kartica.setKartica();
+    // Ulazak na parking
+    // Kada korisnik bude htio izaci unese "da", "1", mozda klikne "Enter"???
+    // Hvala na posjeti????? Da li imate nekih zalbi, ako da mozete ih ovdje podnijeti
+    // Obracunava cijenu i pita za nacin placanja
+    // Bankarska kartica
+    // Mjesecna kartica
+    // Invalidska kartica
+    return 0;
+}
