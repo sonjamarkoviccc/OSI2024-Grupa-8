0c9c804d73407aa6dba472816dae9610fd11daa1
magdalenamilosavljevic <magim7777@gmail.com>
2025-01-10 18:28:15 +0100

    Update Uprava.h
    dodati delovi za cenovnik i izvestaj

--- src/Uprava.h
+++ src/Uprava.h
@@ -2,6 +2,10 @@
 #include <cstdio>
 #include "Nalog.h"
 #include "UpravljanjeNalozima.h"
+#include "MjesecnaKartica.h"
+#include "InvalidskaKartica.h"
+#include "Izvjestaj.h"
+#include "CjenovnikParkinga.h"
 
 class Uprava
 {
@@ -12,9 +16,27 @@ public:
     Uprava() {}
     ~Uprava() {}
 
-    void definisanjeCenovnika();
-    void izvestajORadu();
-    void izdavanjeMjesecneGodisnjeKarte();
+    void definisanjeCjenovnika(CjenovnikParkinga& cjenovnik)
+    {
+        cjenovnik.pokreniProgram();
+    }
+    void izvjestajORadu(Izvjestaj& izvjestaj)
+    {
+        izvjestaj.pokreniProgram();
+    }
+    void izdavanjeMjesecneKarte(MjesecnaKartica& mjesecna)
+    {
+        mjesecna.izdajaKartice();
+        std::cout << "Uspjesno dodata mjesecna kartica.\n" << "Ime: " << mjesecna.getIme() << "\n"
+                  << "Prezime: " << mjesecna.getPrezime() << "\n" << "Datum Izdaje: " << mjesecna.getDatumIzdaje() << "\n"
+                  << "Datum Isteka: " << mjesecna.getDatumIsteka() << std::endl;
+    }
+    void izdavanjeInvalidskeKarte(InvalidskaKartica& invalidska)
+    {
+        invalidska.izdajaKartice();
+        std::cout << "Uspjesno dodata invalidska kartica.\n" << "Ime: " << invalidska.getIme() << "\n"
+                  << "Prezime: " << invalidska.getPrezime() << "\n" << "Datum Izdaje: " << invalidska.getDatumIzdaje() << std::endl;
+    }
     void upravljanjeNalozimaOpcije(const char *fajl);
     void prijava(const char *fajl);
     void prikaziMogucnostiUprave();
