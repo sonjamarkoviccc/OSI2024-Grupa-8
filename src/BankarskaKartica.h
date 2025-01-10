135dc19180e35631c2e00dbd206dcf92d5da91a7
Sonja Markovic <sonjamarkovicccc@gmail.com>
2025-01-10 12:58:30 +0100

    placanje bankarskom karticom

--- src/BankarskaKartica.h
+++ src/BankarskaKartica.h
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
+#include <fstream>
 
 class BankarskaKartica
 {
     private:
         std::string idKartice;
+        std::string Ime;
+        std::string Prezime;
         double stanje;
 
     public:
-        BankarskaKartica(const std::string& id, double saldo)
-        : idKartice(id), stanje(saldo) {}
+        BankarskaKartica() = default;
 
         const std::string& getIdKartice() const {
             return idKartice;
@@ -31,5 +34,54 @@ class BankarskaKartica
                 }
             }
             return true;
+        }
+
+        bool imaRacun(std::string id)
+        {
+            if (id.substr(0,2) == "02")
+            {
+                std::ifstream file("../files/Banka1.txt");
+                if (!file)
+                {
+                    std::cerr << "Greška prilikom otvaranja fajla.\n";
+                    return;
+                }
+
+                while (file >> idKartice >> Ime >> Prezime >> stanje)
+                {
+                    if (idKartice == id)
+                    return 1;
+                }
+            }
+
+            if (id.substr(0,2) == "32")
+            {
+                std::ifstream file("../files/Banka2.txt");
+                if (!file)
+                {
+                    std::cerr << "Greška prilikom otvaranja fajla.\n";
+                    return;
+                }
+
+                while (file >> idKartice >> Ime >> Prezime >> stanje)
+                {
+                    if (idKartice == id)
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+
+        void placanje(double iznos)
+        {
+            if (stanje >= iznos)
+            {
+                stanje -= iznos;
+            }
+            else
+            {
+                std::cout << "Nemate dovoljno na stanju.\n" << std::endl;
+            }
         }
 };
