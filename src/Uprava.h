4cb30255c2e27bed7b354b339e2c7929a856b22d
magdalenamilosavljevic <magim7777@gmail.com>
2025-01-12 18:42:03 +0100

    Update Uprava.h

--- src/Uprava.h
+++ src/Uprava.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdio>
+#include <string>
 #include "Nalog.h"
 #include "UpravljanjeNalozima.h"
 #include "MjesecnaKartica.h"
@@ -16,14 +17,85 @@ public:
     Uprava() {}
     ~Uprava() {}
 
-    void definisanjeCjenovnika(CjenovnikParkinga& cjenovnik)
+    void definisanjeCjenovnika(CjenovnikParkinga& cenovnik)
     {
-        cjenovnik.pokreniProgram();
+         int izbor;
+        do {
+            cenovnik.prikaziMeni();
+            std::cin >> izbor;
+            std::cin.ignore();
+
+            if (izbor == 1) {
+                std::string zona, dan, vrijeme, cijena;
+                std::cout << "Unesite zonu: ";
+                std::getline(std::cin, zona);
+                std::cout << "Unesite dan (radni, vikend, praznik): ";
+                std::getline(std::cin, dan);
+                std::cout << "Unesite vrijeme (u formatu HH:MM:SS): ";
+                std::getline(std::cin, vrijeme);  
+                std::cout << "Unesite cijenu: ";
+                std::getline(std::cin, cijena);
+
+                cenovnik.postaviCijenu(zona, dan, vrijeme, cijena);
+
+            } else if (izbor == 2) {
+                std::string zona, dan, vrijeme;
+                std::cout << "Unesite zonu: ";
+                std::getline(std::cin, zona);
+                std::cout << "Unesite dan (radni, vikend, praznik): ";
+                std::getline(std::cin, dan);
+                std::cout << "Unesite vrijeme (u formatu HH:MM:SS): ";
+                std::getline(std::cin, vrijeme);
+
+                cenovnik.nadjiCijenu(zona, dan, vrijeme);
+
+            } else if (izbor != 3) {
+                std::cout << "Nepoznata opcija.\n";
+            }
+        } while (izbor != 3);
     }
-    void izvjestajORadu(Izvjestaj& izvjestaj)
-    {
-        izvjestaj.pokreniProgram();
-    }
+    void izvjestajORadu(Izvjestaj& izvestaj){
+    int opcija;
+    do {
+        izvestaj.prikaziMeni();
+        std::cin >> opcija;
+
+        switch (opcija) {
+            case 1: {
+                std::string datum;
+                std::cout << "Unesite datum (DD.MM.YYYY): ";
+                std::cin >> datum;
+                izvestaj.generisiDnevniIzvjestaj(datum);
+                break;
+            }
+            case 2: {
+                std::string startDatum, endDatum;
+                std::cout << "Unesite pocetni datum (DD.MM.YYYY): ";
+                std::cin >> startDatum;
+                std::cout << "Unesite krajnji datum (DD.MM.YYYY): ";
+                std::cin >> endDatum;
+                izvestaj.generisiNedeljniIzvjestaj(startDatum, endDatum);
+                break;
+            }
+            case 3: {
+                int mjesec, godina;
+                std::cout << "Unesite mjesec (1-12): ";
+                std::cin >> mjesec;
+                std::cout << "Unesite godinu (YYYY): ";
+                std::cin >> godina;
+                izvestaj.generisiMjesecniIzvjestaj(mjesec, godina);
+                break;
+            }
+            case 4:
+                std::cout << "Izlaz...\n";
+                break;
+            default:
+                std::cout << "Nepoznata opcija. Pokusajte ponovo.\n";
+                break;
+        }
+    } while (opcija != 4);
+}
+
     void izdavanjeMjesecneKarte(MjesecnaKartica& mjesecna)
     {
         mjesecna.izdajaKartice();
